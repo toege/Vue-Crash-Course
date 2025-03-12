b72fc9928281220e6cba64d62670c6ce26138c8c
Tonnes <to.smith.eg@gmail.com>
2025-03-12 12:49:09 +0100

    Fundamentals fetch

--- vue3-crash-course/src/App.vue
+++ vue3-crash-course/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 const name = ref('Jonas Donas');
 const status = ref('active');
 const tasks = ref(['task 1', 'task 2', 'task 3']);
@@ -27,6 +27,16 @@ const addTask = () => {
 const deleteTask = (index) => {
   tasks.value.splice(index, 1);
 };
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title)
+  } catch (error) {
+    
+  }
+})
 </script>
 
 <template>
